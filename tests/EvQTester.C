5b4f177e4a61552852daff9e6df2851e78cb001a
jjhyvone <jjhyvone>
2005-11-02 12:12:15 +0000

    Nothing really.

--- tests/EvQTester.C
+++ tests/EvQTester.C
@@ -1,6 +1,6 @@
 
 #define NDEBUG
-#define GNU_PREFETCH
+//#define GNU_PREFETCH
 
 #include <iostream>
 #include <cmath>
@@ -10,52 +10,70 @@
 #include <ctime>
 
 #define NUM_RANDS 10000000
+#define STREAK 1000
 #define Q_SIZE 1000000
 #define expec 10000000.0
 #include <cassert>
 
-class tst: public EvQueue<unsigned>::Event {
+typedef unsigned /*long*/ TimeType;
+
+
+class tst: public EvQueue<TimeType>::Event {
 public:
-  virtual bool foo() {return false;}
+  virtual bool foo() {
+    //std::cerr << "Foo!";
+    return false;
+  }
 };
 
 class tst2: public tst {
 public:
-  virtual bool foo() {return true;}
+  virtual bool foo() {
+    //std::cerr << "BAr!";
+    return true;
+  }
 };
 
 
+//typedef EvQueue<TimeType>::Event Event;
 typedef tst Event;
 typedef Event * EventPtr;
 
-
 int main() {
   std::cerr << sizeof(Event) << " " << sizeof(tst);
-  EvQueue<unsigned> queue(0,20);
+  EvQueue<TimeType> queue(0,20);
 #ifndef  NDEBUG
-  AutoMap<unsigned, unsigned> hash;
+  AutoMap<TimeType, unsigned> hash;
 #endif
   
-  unsigned * addTimes;
+  TimeType * addTimes;
 
   clock_t cpustart;
+  //clock_t tempcpu=0;
+  clock_t pushTot=0;
+  clock_t popTot=0;
 
-  unsigned lastTime=0;
+  TimeType lastTime=0;
   float recinvlambda=-expec;
 
   tst test;
   std::cerr << (&test) << " ";
   std::cerr << (&(test.next)) << "\n";
+  
+  EvQueue<TimeType>::Event * ptr=(&test);
+  std::cerr << ptr << "\n";
 
-  EvQueue<unsigned>::Event test2;
+  tst * foo=(tst *) ptr;
+  std::cerr << foo << "\n";
+
+  EvQueue<TimeType>::Event test2;
   std::cerr << (&test2) << " ";
   std::cerr << (&(test2.next)) << "\n";
 
   std::cerr << "To rands\n";
-
-  addTimes=new unsigned[NUM_RANDS];
+  addTimes=new TimeType[NUM_RANDS];
   for (unsigned i=0; i<NUM_RANDS; ++i) {
-    addTimes[i]=((unsigned) (recinvlambda*log(((float) rand())/RAND_MAX)));
+    addTimes[i]=((TimeType) (recinvlambda*log(((float) rand())/RAND_MAX)));
   }
   std::cerr << "Into the test\n";
 
@@ -63,12 +81,13 @@ int main() {
 
   for (i=0; i<Q_SIZE; i++) {
     //std::cerr << "#";
-    EventPtr curr;
-    if (i & 1 == 0) 
+    EventPtr curr=new Event;;
+    if ((i & 1) == 0) {
       curr=new tst();
-    else 
+    } else { 
       curr=new tst2();
-    
+    }    
+
     assert(queue.getNumEvents()==i);
     queue.push(curr,addTimes[i]);
     //queue.printout();
@@ -84,10 +103,12 @@ int main() {
   unsigned acc=0;
 
   for (; i<NUM_RANDS; i++) {
-    unsigned currTime;
+
+    TimeType currTime;
     assert(queue.getNumEvents()==Q_SIZE);
     //std::cerr << "#";
     EventPtr ptr=(EventPtr) queue.pop(currTime);
+    //std::cout << currTime << "\n";
     acc+=ptr->foo();
     //delete ptr;
     //std::cerr << "Pop:" << currEvent << "\n";
@@ -95,6 +116,7 @@ int main() {
     lastTime=currTime;
     assert(hash[currTime]-- > 0); /* Was not empty*/
     currTime+=addTimes[i];
+    //currTime+=((TimeType) (recinvlambda*log(((float) rand())/RAND_MAX)));
     //ptr=new Event();
     queue.push(ptr, currTime);
     //std::cerr << "+";
@@ -106,31 +128,54 @@ int main() {
   cpustart=clock(); 
   std::cerr << cpustart << "\n";
 
+//   for (i=0; i<NUM_RANDS; i+=STREAK) {
+//     TimeType currTime;
+//     tempcpu=clock();
+//     for (unsigned j=i; j<i+STREAK; ++j) {
+//       delete queue.pop(currTime);
+//     }
+//     //std::cerr << i << "\n";
+//     popTot+=clock();
+//     popTot-=tempcpu;
+//     tempcpu=clock();
+//     for (unsigned j=i; j<i+STREAK; ++j) {
+//       queue.push(new Event(), currTime+addTimes[j]);
+//       //std::cerr << "+";
+//     }
+//     pushTot += clock();
+//     pushTot -= tempcpu;
+//   }
+
   for (i=0; i<NUM_RANDS; i++) {
-    unsigned currTime;
+    TimeType currTime;
     assert(queue.getNumEvents()==Q_SIZE);
     //std::cerr << "#";
     EventPtr ptr=(EventPtr) queue.pop(currTime);
+    //acc+=ptr->foo();
     //delete ptr;
     //std::cerr << "Pop:" << currEvent << "\n";
     assert(currTime >= lastTime);
     lastTime=currTime;
     assert(hash[currTime]-- > 0); /* Was not empty*/
+    //currTime+=((TimeType) (recinvlambda*log(((float) rand())/RAND_MAX)));
     currTime+=addTimes[i];
     //ptr=new Event();
     queue.push(ptr, currTime);
     //std::cerr << "+";
     assert(++hash[currTime] > 0);
   }
-  
+
   std::cerr << "\nClocks" << (clock()-cpustart) << "Per sec:" << CLOCKS_PER_SEC;
-  std::cerr << clock() << "\n";
-  std::cerr << "\nHold per sec:" 
-	    << ((float) (NUM_RANDS)*CLOCKS_PER_SEC)/(clock()-cpustart) 
+  std::cerr << "\n" << clock() << "\n";
+  std::cerr << "\nHolds per musec" 
+	    << (((float) (NUM_RANDS))/(clock()-cpustart))
 	    << "\n";
 
+  std::cerr << "Pushes per musec" << (((float) (NUM_RANDS))/pushTot) << "\n";
+  std::cerr << "Pops per musec" << (((float) (NUM_RANDS))/popTot) << "\n";
+
   for (i=Q_SIZE; i>0; i--) {
-    unsigned currTime;
+    TimeType currTime;
     assert(queue.getNumEvents()==i);
     //std::cerr << "#";
     EventPtr ptr=(EventPtr) queue.pop(currTime);
